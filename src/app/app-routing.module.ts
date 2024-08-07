a922793055c0f672d5dd7ea1fb7163d395a3e18e
Cesar <129460134+CesarrAnn@users.noreply.github.com>
2024-08-07 02:16:28 -0500

    Update files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
+import { PostsComponent } from './placeholder/pages/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostsComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: '**', component: PageNotFoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
+import { PostsComponent } from './placeholder/pages/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
