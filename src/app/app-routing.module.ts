34beec4b9eb8638e16a299883be6a9197300b4f8
Cesar <129460134+CesarrAnn@users.noreply.github.com>
2024-08-09 00:50:42 -0500

    Update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
 import { PostsComponent } from './placeholder/pages/posts/posts.component';
+import { PhotosComponent } from './placeholder/pages/photos/photos.component';
+import { HomeComponent } from './placeholder/pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: PostsComponent },
+  { path: '', component: HomeComponent },
   { path: 'posts', component: PostsComponent },
+  { path: 'photos', component: PhotosComponent },
   { path: '**', component: PageNotFoundComponent }
 
 ];
