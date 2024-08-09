fa80852ec714e1ecdfdba0bb0b180ab4f4b87bec
Cesar <129460134+CesarrAnn@users.noreply.github.com>
2024-08-09 00:49:21 -0500

    Added posts services

--- src/app/placeholder/services/posts/posts.service.ts
+++ src/app/placeholder/services/posts/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BaseService } from '../../../shared/base.service';
 
@@ -14,7 +14,21 @@ export class PostsService {
   }
 
   getPosts() {
-    return this.http.get(`${this.serverBasePath}/posts`);
+    return this.http.get(`${this.serverBasePath}/posts?_limit=10`);
+  }
+
+  createPost(postData: { title: string, body: string, userId: number }) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post(`${this.serverBasePath}/posts`, postData, { headers });
+  }
+
+  deletePost(postId: number) {
+    return this.http.delete(`${this.serverBasePath}/posts/${postId}`);
+  }
+
+  updatePost(postId: number, postData: { title: string, body: string, userId: number }) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.put(`${this.serverBasePath}/posts/${postId}`, postData, { headers });
   }
   
 }
