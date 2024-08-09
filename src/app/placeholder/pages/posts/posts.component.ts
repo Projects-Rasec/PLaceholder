d8a693be8f2d9b78d34c031fb15ba9fea3a7cf97
Cesar <129460134+CesarrAnn@users.noreply.github.com>
2024-08-09 00:51:23 -0500

    Update posts component

--- src/app/placeholder/pages/posts/posts.component.ts
+++ src/app/placeholder/pages/posts/posts.component.ts
@@ -9,12 +9,34 @@ import { PostsService } from '../../services/posts/posts.service';
 export class PostsComponent implements OnInit {
 
   posts:any[] = [];
+  createPost = { title: '', body: '', userId: 1 };
+  updatePost = { id: 0, title: '', body: '', userId: 1 };
+  deletePostId = { id: 0 };
+  response:any;
 
   constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
     this.postsService.getPosts().subscribe((posts:any) => {
       this.posts = posts;
+    });
+  }
+
+  submitCreatePost():void {
+    this.postsService.createPost(this.createPost).subscribe(response => {
+      this.response = response;
+    })
+  }
+
+  deletePost(postId:number):void {
+    this.postsService.deletePost(postId).subscribe(response => {
+      this.response = response;
+    });
+  }
+
+  submitUpdatePost(postId:number):void {
+    this.postsService.updatePost(postId, this.updatePost).subscribe(response => {
+      this.response = response;
     });
   }
 
