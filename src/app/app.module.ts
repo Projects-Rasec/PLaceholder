2d81490152d9d5f31517a808ee5dc9b4c713dd25
Cesar <129460134+CesarrAnn@users.noreply.github.com>
2024-08-09 00:50:20 -0500

    Update modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { PageNotFoundComponent } from './public/pages/page-not-found/page-not-found.component';
 import { PostsComponent } from './placeholder/pages/posts/posts.component';
+import { ToolbarComponent } from './placeholder/components/toolbar/toolbar.component';
+import { PhotosComponent } from './placeholder/pages/photos/photos.component';
+import { HomeComponent } from './placeholder/pages/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    PostsComponent
+    PostsComponent,
+    ToolbarComponent,
+    PhotosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
